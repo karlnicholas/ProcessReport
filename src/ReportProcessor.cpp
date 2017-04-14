1dda52d282956b4fe97bb2773e38e5785e1e2bf6
karlnicholas <karl.nicholas@outlook.com>
2017-04-13 18:56:46 -0700

    Updates working with pointers.

--- src/ReportProcessor.cpp
+++ src/ReportProcessor.cpp
@@ -21,39 +21,52 @@
 using namespace std;
 
 // functions
-vector<PoliceReport> getPoliceReports();
-list<PoliceReport> getPoliceReportList();
-void halfHelper(int first, int mid, int last, set<PoliceReport> &reportTree, vector<PoliceReport> &policeReports);
-void print10RandomReports(vector<PoliceReport> &policeReports, set<PoliceReport> &reportTree);
+vector<PoliceReport*> getPoliceReports();
+//list<PoliceReport> getPoliceReportList();
+bool sortComparePR(PoliceReport * l, PoliceReport * r) {
+	return l->getIncidentNumber() < r->getIncidentNumber();
+}
+
+struct ltPoliceReport {
+	bool operator()(PoliceReport * l, PoliceReport * r) const {
+		return l->getIncidentNumber() < r->getIncidentNumber();
+	}
+};
+
+void halfHelper(int first, int mid, int last, set<PoliceReport*, ltPoliceReport> &reportTree, vector<PoliceReport*> &policeReports);
+void print10RandomReports(long indicentNumbers[], vector<PoliceReport*> &policeReports, set<PoliceReport*, ltPoliceReport> &reportTree);
 
 int main() {
 
     srand ( unsigned ( std::time(0) ) );
 
-	vector<PoliceReport> policeReports = getPoliceReports();
+	vector<PoliceReport*> policeReports = getPoliceReports();
 //	list<PoliceReport> policeReportList = getPoliceReportList();
 
-	set<PoliceReport> reportTree;
+	set<PoliceReport*, ltPoliceReport> reportTree;
 
 	clock_t start = clock();
 
+/*
 	random_shuffle(policeReports.begin(), policeReports.end());
 
 	for ( vector<PoliceReport>::iterator it=policeReports.begin(); it!=policeReports.end(); ++it ) {
 		reportTree.insert(*it);
 	}
+*/
 
-/*
-	policeReportList.sort();
+//	policeReportList.sort();
 
-	vector<PoliceReport> policeReports( policeReportList.begin(), policeReportList.end() );
-	policeReportList.clear();
+//	vector<PoliceReport*> policeReports( policeReport.begin(), policeReport.end() );
+//	policeReportList.clear();
 
-	halfHelper(0, policeReports.size()/2, policeReports.size()-1, 0, reportTree, policeReports );
+
+	sort( policeReports.begin(), policeReports.end(), sortComparePR);
+
+	halfHelper(0, policeReports.size()/2, policeReports.size()-1, reportTree, policeReports );
 	reportTree.insert(policeReports.front());
 	reportTree.insert(policeReports.back());
-*
-*/
+
 	cout << "list count: " << policeReports.size() << endl;
 	cout << "set count: " << reportTree.size() << endl;
 
@@ -61,51 +74,56 @@ int main() {
 	double time = (double) (end-start) / CLOCKS_PER_SEC * 1000.0;
 
 	cout << "Time was " << time << endl;
-	print10RandomReports(policeReports, reportTree);
+	long indicentNumbers[10];
+	for (int i = 0; i < 10; i++) {
+		indicentNumbers[i] = policeReports[i]->getIncidentNumber();
+	}
+	print10RandomReports(indicentNumbers, policeReports, reportTree);
 	return 0;
 }
 
-void print10RandomReports(vector<PoliceReport> &policeReports, set<PoliceReport> &reportTree) {
-	long indicentNumbers[10];
-	for (int i = 0; i < 10; i++) {
-		indicentNumbers[i] = policeReports[i].getIncidentNumber();
-	}
-	sort( policeReports.begin(), policeReports.end() );
+void print10RandomReports(long indicentNumbers[],
+		vector<PoliceReport*> &policeReports,
+		set<PoliceReport*, ltPoliceReport> &reportTree) {
 	for (int i = 0; i < 10; i++) {
 		long incidentNumber = indicentNumbers[i];
 
 		clock_t start = clock();
-		set<PoliceReport>::iterator it = reportTree.find(incidentNumber);
+//		set<PoliceReport*>::iterator it = reportTree.find(incidentNumber);
+		PoliceReport tRep(incidentNumber);
+		set<PoliceReport*>::iterator it = reportTree.find(&tRep);
 		clock_t end = clock();
 		double time = (double) (end-start) / CLOCKS_PER_SEC * 1000.0;
 		cout << "Time for set find was " << time << endl;
-		cout << "Report = " << *it << endl;
+		cout << "Report = " << **it << endl;
 
 		// compare this to iterating through a sorted version of the list
 		start = clock();
-		vector<PoliceReport>::iterator vit = std::find(policeReports.begin(), policeReports.end(), incidentNumber);
+//		vector<PoliceReport>::iterator vit = std::find(policeReports.begin(), policeReports.end(), incidentNumber);
+		vector<PoliceReport*>::iterator vit = lower_bound(policeReports.begin(), policeReports.end(), &tRep, sortComparePR);
+
 		end = clock();
 		time = (double) (end-start) / CLOCKS_PER_SEC * 1000.0;
 		cout << "Time for vector find was " << time << endl;
-		cout << "Report = " << *vit << endl;
+		cout << "Report = " << **vit << endl;
 
 	}
 
 }
 
-void halfHelper(int first, int mid, int last, set<PoliceReport> &reportTree, vector<PoliceReport> &policeReports) {
+void halfHelper(int first, int mid, int last, set<PoliceReport*, ltPoliceReport> &reportTree, vector<PoliceReport*> &policeReports) {
 	if( (last - first) <= 1 ) return;
 	reportTree.insert(policeReports[mid]);
 	halfHelper(first, first + (mid-first)/2, mid, reportTree, policeReports);
 	halfHelper(mid, mid + (last-mid)/2, last, reportTree, policeReports);
 }
 
-vector<PoliceReport> getPoliceReports() {
+vector<PoliceReport*> getPoliceReports() {
 	string line;
 	ifstream myfile("SFPD_Incidents_TheftLarceny.csv");
 	int lineCount = 0;
 	string temp[8];
-	vector<PoliceReport> policeReports;
+	vector<PoliceReport*> policeReports;
 
 	if (myfile.is_open()) {
 		while (std::getline(myfile, line)) {
@@ -114,10 +132,13 @@ vector<PoliceReport> getPoliceReports() {
 			while (getline(iss, temp[i], ',')) {
 				i++;
 			}
-			PoliceReport report(atol(temp[0].c_str()), temp[1], temp[2],
-					atoi(temp[3].c_str()), temp[4], temp[5], temp[6], temp[7]);
-			policeReports.push_back(report);
-			lineCount++;
+			long incidentNum = atol(temp[0].c_str());
+			if ( incidentNum != 0 ) {
+				PoliceReport* report = new PoliceReport(incidentNum, temp[1], temp[2],
+						atoi(temp[3].c_str()), temp[4], temp[5], temp[6], temp[7]);
+				policeReports.push_back(report);
+				lineCount++;
+			}
 		}
 		myfile.close();
 	}
@@ -128,7 +149,7 @@ vector<PoliceReport> getPoliceReports() {
 	return policeReports;
 }
 
-
+/*
 list<PoliceReport> getPoliceReportList() {
 	string line;
 	ifstream myfile("SFPD_Incidents_TheftLarceny.csv");
@@ -156,3 +177,4 @@ list<PoliceReport> getPoliceReportList() {
 
 	return policeReports;
 }
+*/
